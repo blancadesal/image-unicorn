c9c75a09b29d19dbfc817a70d324b3fe8697c6ea
Slavina Stefanova <sstefanova@wikimedia.org>
2022-05-12 11:33:50 +0200

    Break up transforms some more

--- src/helpers.py
+++ src/helpers.py
@@ -21,16 +21,19 @@ def transform_image(image_bytes):
                                         transforms.CenterCrop(224),
                                         ])
     my_transforms2 = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize(
+                                        ])
+    my_transforms3 = transforms.Compose([transforms.Normalize(
                                             [0.485, 0.456, 0.406],
                                             [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
     logger.debug("Image is ready for transform!")
     result_image1 = my_transforms1(image)
-    logger.debug("Transform1 applied!")
+    logger.debug("Transform1 applied! (Resize, CenterCrop)")
     result_image2 = my_transforms2(result_image1)
-    logger.debug("Transform2 applied!")
-    return result_image2.unsqueeze(0)
+    logger.debug("Transform2 applied! (ToTensor)")
+    result_image3 = my_transforms3(result_image2)
+    logger.debug("Transform3 applied! (Normalize)")
+    return result_image3.unsqueeze(0)
 
 
 def format_class_name(class_name):
