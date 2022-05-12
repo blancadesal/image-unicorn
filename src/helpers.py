8f3a33b6c261445915583a958a39f65125ea8e67
Slavina Stefanova <sstefanova@wikimedia.org>
2022-05-12 11:28:31 +0200

    Break up transforms

--- src/helpers.py
+++ src/helpers.py
@@ -17,18 +17,20 @@ def get_model():
 
 def transform_image(image_bytes):
     logger.debug('Trying to transform the image!')
-    my_transforms = transforms.Compose([transforms.Resize(255),
+    my_transforms1 = transforms.Compose([transforms.Resize(255),
                                         transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
+                                        ])
+    my_transforms2 = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize(
                                             [0.485, 0.456, 0.406],
                                             [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
-    if image:
-        logger.debug("Got image inside transform!")
-    result_image = my_transforms(image).unsqueeze(0)
-    logger.debug("result_image OK!")
-    return result_image
+    logger.debug("Image is ready for transform!")
+    result_image1 = my_transforms1(image)
+    logger.debug("Transform1 applied!")
+    result_image2 = my_transforms2(result_image1)
+    logger.debug("Transform2 applied!")
+    return result_image2.unsqueeze(0)
 
 
 def format_class_name(class_name):
